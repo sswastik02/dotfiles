564f7e2925f7c99e69017c0bc1f81bc5061316be
sswastik02 <40518186+sswastik02@users.noreply.github.com>
2024-06-30 12:38:25 +0530

    Config files for `alacritty`, `ly`, `rofi`, `sway` and `swaylock`

---
+++ sway/bar.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+linux=$(uname -a | cut -d ' ' -f3)
+hostname=$(hostname)
+
+while true 
+do
+	d=$(date +'%Y-%m-%d 🗓️ ┆ %X ⌛')
+	vol=$(pamixer --get-volume)
+	up=$(uptime | cut -d ',' -f1 | cut -d ' ' -f4,5)
+	bat=$(cat /sys/class/power_supply/BAT0/capacity)
+	bat_status=$(cat /sys/class/power_supply/BAT0/status)
+	interface=$(ip route | awk '/default/ {print $5}')
+	ip=$(hostname -I | awk '{print $1}')
+
+	rx_bytes=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
+	tx_bytes=$(cat /proc/net/dev | grep $interface | awk '{print $10}')
+
+
+	sleep 1
+
+	rx_bytes_new=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
+	tx_bytes_new=$(cat /proc/net/dev | grep $interface | awk '{print $10}')
+
+	rx_speed=$(( ($rx_bytes_new - $rx_bytes) / 1024 ))
+	tx_speed=$(( ($tx_bytes_new - $tx_bytes) / 1024 ))
+	_
+
+	echo "$hostname ($ip) 📡 ┆ $linux 🐧 ┆ $up 🚀 ┆ $rx_speed KB/s ⬇️ $tx_speed KB/s ⬆️ ┆ $vol% 🔊 ┆ $d  ┆ $bat% 🔋[$bat_status] "  
+done
