63ad403c79834a150fe22147152d02ec55a0c5a9
sswastik02 <40518186+sswastik02@users.noreply.github.com>
2024-11-04 17:05:29 +0530

    Add Refactoring and tabs in nvim with plugin for ssh-agent in tmux
    Signed-off-by: sswastik02 <40518186+sswastik02@users.noreply.github.com>

--- nvim/lua/sswastik02/core/keymaps.lua
+++ nvim/lua/sswastik02/core/keymaps.lua
@@ -16,7 +16,20 @@ vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open a new buffer
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<leader>t0", "<cmd>tabl<CR>", { desc = "Prev Buffer" })
 
+local function goto_tab(num)
+  local tabnr = tonumber(num)
+  if tabnr and tabnr > 0 then
+    vim.cmd('tabnext ' .. tabnr)
+  else
+    print('Invalid tab number: ' .. num)
+  end
+end
+
+for i = 1, 9 do
+  vim.keymap.set('n', '<leader>t'..i, function() goto_tab(i) end, { noremap = true, silent = true })
+end
 -- system clipboard
 vim.keymap.set("n", "<leader>p", "\"+p", { desc = "copy to system clipboard" })
 vim.keymap.set("v", "<leader>y", "\"+y", { desc = "paste from system clipboard" })
---
+++ nvim/lua/sswastik02/plugins/refactoring.lua
@@ -0,0 +1,17 @@
+  return {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    lazy = false,
+    config = function()
+      require("refactoring").setup()
+      require("telescope").load_extension("refactoring")
+      vim.keymap.set(
+          {"n", "x"},
+          "<leader>rr",
+          function() require('telescope').extensions.refactoring.refactors() end
+      )
+    end
+}
