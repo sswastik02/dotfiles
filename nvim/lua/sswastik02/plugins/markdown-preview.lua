90066f371e24665eb78a04f1877d5478b99f1f53
sswastik02 <sswastik02@gmail.com>
2025-08-30 11:34:49 +0530

    Render markdown inside nvim itself

--- nvim/lua/sswastik02/plugins/markdown-preview.lua
+++ nvim/lua/sswastik02/plugins/markdown-preview.lua
@@ -1,10 +1,16 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    init = function()
-      vim.g.mkdp_browser = "firefox"
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    config = function ()
+      local rm = require("render-markdown")
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+      map('n', '<leader>mrt',rm.set)
     end
 }
